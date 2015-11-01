baa28b3ab3d9ad1f120172aff1f888ccf936f6aa
Jon Clayton <Jonclayton@swbell.net>
2015-10-31 19:06:01 -0500

    Update my_solution.rb

--- my_solution.rb
+++ my_solution.rb
@@ -21,17 +21,22 @@ end
 
 # Person 2
 def my_array_modification_method!(source, thing_to_modify)
-# This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.dup
+  i_want_pets.map!{|number| number.is_a?(Integer) ? number + 1 : number}
 end
 
 def my_hash_modification_method!(source, thing_to_modify)
- # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.each do |name,age|
+    source[name] = age + thing_to_modify
+  end
+  return source
 end
 
 # Identify and describe the Ruby method(s) you implemented.
-#
-#
-#
+# .map!  - This method just inputs new values into an array.
+# is_a?  - This checks if an element is a certain thing, so is_a?(Integer) checks if an element is an integer.
+# .each  - This just iterates through an array or hash.
+
 
 
 # Person 3
@@ -79,6 +84,22 @@ end
 #
 
 
+# Person 2: Trevor Newcomb
+
+# Release 1: Identify and describe the Ruby method you implemented. Teach your
+# accountability group how to use the methods.
+# I used .map!, .is_a?, and .each
+#  .map!  - This method just inputs new values into an array.
+#   is_a?  - This checks if an element is a certain thing, so is_a?(Integer) checks if an element is an integer.
+#   .each  - This just iterates through an array or hash.
+
+
+# Release 3: Reflect!
+# What did you learn about researching and explaining your research to others?
+# I learned that the best way to parse Ruby Docs is to really know what kind of method you need by looking for it in another way, like stack overflow, and then instead of looking at answers people have posted, just take away some recommended methods to use and try them out to see how they work, using Ruby Docs as a guideline.
+
+# Person 5: Jon Clayton
+
 # Release 1: Identify and describe the Ruby method you implemented. Teach your
 # accountability group how to use the methods.
 # #partion works elegantly to separate a set of values into two array.
@@ -93,3 +114,4 @@ end
 # I think explaining is all about illustrating the use of methods, ideally in a
 # way that prove the method is useful to the audience. A written description is 
 # otherwise unlikely to receive enough focus to sink in.
+
